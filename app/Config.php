262673688a921fd22eaee0611f5397f19854c4f7
Alessandro Rodríguez <alessandro@secmotic.com>
2020-11-29 22:48:53 +0100

    nombre de la bd

--- app/Config.php
+++ app/Config.php
@@ -3,7 +3,7 @@
  class Config
  {
      static public $mvc_bd_hostname = "mysql.alessrodrgz.com.mialias.net";
-     static public $mvc_bd_nombre   = "clases-daw";
+     static public $mvc_bd_nombre   = "alimentos42020";
      static public $mvc_bd_usuario  = "mysqlremote";
      static public $mvc_bd_clave    = "practicadaw2020";
      static public $mvc_vis_css     = "estilo.css";
