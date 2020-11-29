dc5a0f713fe3bfe288e07ede2b3635d141126c10
Alessandro Rodríguez <alessandro@secmotic.com>
2020-11-29 22:45:37 +0100

    archivo de configuración

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "mysql.alessrodrgz.com.mialias.net";
+     static public $mvc_bd_nombre   = "clases-daw";
+     static public $mvc_bd_usuario  = "mysqlremote";
+     static public $mvc_bd_clave    = "practicadaw2020";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
